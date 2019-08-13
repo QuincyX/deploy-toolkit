a27fd4269e47239470870f2eb4838761cb81a506
QuincyX <likequincy@outlook.com>
2019-08-13 18:23:23 +0800

    u

--- src/deploy.js
+++ src/deploy.js
@@ -44,40 +44,47 @@ function handleFilePath(obj, type) {
 /**
  * 上传文件
  **/
-function uploadFile() {
+function uploadFile(options) {
   Object.keys(staticFilesPath).forEach(key => {
     handleFilePath(staticFilesPath[key], key)
   })
   const tasks = localFileJson.map(item => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       if (item.type == 'folder') {
+        if (options.deleteSubDir) {
+          await sftp.rmdir(item.remotePath, true).then(() => {
+            console.log(`${item.remotePath}目录删除成功`)
+          })
+        }
         sftp
           .mkdir(item.remotePath, false)
           .then(res => {
-            console.log(`${item.localPath}目录创建成功`)
+            console.log(`${item.remotePath}目录创建成功`)
             resolve()
           })
           .catch(err => {
-            console.log(`${item.localPath}目录创建失败`)
+            console.log(`${item.remotePath}目录创建失败`)
+            console.log(err)
             resolve()
           })
       } else {
         sftp
-          .put(item.localPath, item.remotePath)
+          .fastPut(item.localPath, item.remotePath)
           .then(() => {
-            console.log(`${item.localPath}上传完成`)
+            console.log(`${item.remotePath}上传完成`)
             resolve()
           })
           .catch(err => {
-            console.log(`${item.localPath}上传失败`)
+            console.log(`${item.remotePath}上传失败`)
+            console.log(err)
             reject()
           })
       }
     })
   })
-  console.log('开始上传...')
   return Promise.all(tasks)
 }
+let startTime = Date.now()
 const deploy = function(options) {
   return new Promise((resolve, reject) => {
     staticFilesPath.folder = {
@@ -87,23 +94,29 @@ const deploy = function(options) {
     sftp
       .connect(options.server)
       .then(async data => {
+        startTime = Date.now()
         console.log('ssh服务器连接成功！')
         if (options.backupRemotePath) {
           console.log(`备份原目录并创建新目录`)
           const bak_name = `${options.remotePath}-${new Date().getTime()}`
           await sftp.rename(options.remotePath, bak_name)
           await sftp.mkdir(options.remotePath, false)
+        } else if (options.deleteRemotePath) {
+          await sftp.rmdir(options.remotePath, true)
         }
-        console.log('正在上传...')
-        return uploadFile()
+        console.log('开始上传...')
+        return uploadFile(options)
       })
       .then(res => {
         console.log('------所有文件上传完成!-------\n')
+        console.log('上传文件耗时：');
+
         sftp.end()
         resolve()
       })
       .catch(err => {
         console.error('------上传失败,请检查!-------\n')
+        console.error(err)
         sftp.end()
         reject(err)
       })
--- src/webhook.js
+++ src/webhook.js
@@ -1,8 +1,12 @@
 const axios = require('axios')
 
 const webhook = option => {
-  console.log('------开始执行webhook-------\n')
-  return axios.post(option.url, option.content)
+  if (option.enable) {
+    console.log('------开始执行webhook-------\n')
+    return axios.post(option.url, option.content)
+  } else {
+    return Promise.resolve()
+  }
 }
 
 module.exports = webhook
